d0afc334a6d1277b70f8b3fe45d233c273bb219b
myMapleStory <mrraindrop2012@gmail.com>
2016-01-11 16:01:12 +0800

    第九章 service

--- app/src/main/java/com/example/maple/servicetest/MainActivity.java
+++ app/src/main/java/com/example/maple/servicetest/MainActivity.java
@@ -1,13 +1,70 @@
 package com.example.maple.servicetest;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private MyService.DownloadBinder downloadBinder;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            downloadBinder = (MyService.DownloadBinder) service;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+        }
+    };
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button startService = (Button) findViewById(R.id.button);
+        Button stopService = (Button) findViewById(R.id.button2);
+        final Button bindService = (Button) findViewById(R.id.button3);
+        final Button unbindService = (Button) findViewById(R.id.button4);
+
+        startService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(MainActivity.this, MyService.class);
+                startService(startIntent);
+            }
+        });
+
+        stopService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent stopIntent = new Intent(MainActivity.this, MyService.class);
+                stopService(stopIntent);
+            }
+        });
+
+        bindService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bindIntent = new Intent(MainActivity.this, MyService.class);
+                bindService(bindIntent, connection, BIND_AUTO_CREATE);
+            }
+        });
+
+        unbindService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                unbindService(connection);
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/example/maple/servicetest/MyService.java
+++ app/src/main/java/com/example/maple/servicetest/MyService.java
@@ -2,15 +2,49 @@ package com.example.maple.servicetest;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
 /**
  * Created by pc1 on 2016/1/11.
  */
 public class MyService extends Service {
 
+    private DownloadBinder mBinder = new DownloadBinder();
+
+    class DownloadBinder extends Binder{
+
+        public void startDownload(){
+            Log.d("MyService", "startDownload executed");
+        }
+
+        public int getProgress(){
+            Log.d("MyService","getProgress executed");
+            return 0;
+        }
+    }
+
     @Override
     public IBinder onBind(Intent intent){
-        return null;
+        return mBinder;
+    }
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        Log.d("MyService", "onCreate executed");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        Log.d("MyService", "onStartCommand executed");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d("MyService", "onDestroy executed");
     }
 }
